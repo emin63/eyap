a4e6b3169e94a9cc376403ae79ffa9e75799acbf
Emin Martinian <emin.martinian@gmail.com>
2017-09-02 17:04:53 -0400

    Bumped verion number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_readme():
 
 setup(
     name='eyap',
-    version='0.7.1',
+    version='0.7.2',
     description='Tools for extending yapping and comment management',
     long_description=get_readme(),
     url='http://github.com/emin63/eyap',
